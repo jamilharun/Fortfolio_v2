d56f6d709986464f9ee312d7c53ded3408a02818
Jamil Harun <jamilharunl45@gmail.com>
2023-08-26 19:51:20 +0800

    part1 done

---
+++ src/app.js
@@ -0,0 +1,31 @@
+// checking window width
+console.log(window.innerWidth); //full window width
+
+// onUpdate window width when you inspect it
+window.addEventListener('resize', () =>{console.log(window.innerWidth);});
+
+
+
+// gsap configuration
+
+
+
+// gsap animations
+const tl = gsap.timeline({stagger: true})
+tl 
+    .to('.img', {
+        // opacity: 0,
+        // duration: 2,
+        // scale: 1.1,
+        // rotate:10,
+        // ease: "power2.out"
+    })
+    .to('.img', {
+        duration: 3,
+        scale: 1.05,
+        rotate: 10,
+        ease: 'power2.out'
+    })
+    .to('.img', {
+        opacity: 0
+    })
