74251545f0b2cd64d225e505b4ba590edf20c5a0
Jamil Harun <jamilharunl45@gmail.com>
2023-08-29 21:39:17 +0800

    some updates

--- src/app.js
+++ src/app.js
@@ -12,14 +12,22 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(Flip);
 
 // gsap animations
+gsap.to('.jamil',{duration:3,opacity:1});
+
 const tl = gsap.timeline({stagger: true})
 tl  .to('.img', {duration: 3,scale: 1.05,rotate: 10,ease: 'power2.out'})
-    .to('.img', {opacity: 0});
+    .to('.img', {opacity: 0})
+    .to('.jamil', {opacity: 0, delay: 3, ease: 'power2.out' });
+
+//  remember jamil
+//  mag lalagay ka ng 2 or 3 pics
+//  gagawin mong random ang pag reveal ng pics 
+
 
 // automatic redirect
 let count = 1;
 setInterval(()=>{
     count++
-    if(count > 6) location.replace('/src/components/home.html')
+    if(count > 9) location.replace('/src/components/home.html')
 }, 1000)
 
