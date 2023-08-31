61b58dc3b3ec7a24a2754c45b13d4a7ccb7260fc
Jamil Harun <jamilharunl45@gmail.com>
2023-08-31 22:03:30 +0800

    home

--- src/components/scripts/home.js
+++ src/components/scripts/home.js
@@ -1,1 +1,9 @@
-const swup = new Swup()
+// const swup = new Swup()
+// random number generator
+var randNum = Math.floor(Math.random() * 1) + 1;
+console.log(randNum);
+
+// gsap animation
+if(randNum == 1){
+    gsap.to
+}
