0e7eaa8c3c194826ea704e1d01cf7602b6536b65
jamilharun <jamilharunl45@gmail.com>
2024-10-09 17:36:13 +0800

    asdasdasd

--- src/app.js
+++ src/app.js
@@ -2,33 +2,30 @@
 console.log(window.innerWidth); //full window width
 
 // onUpdate window width when you inspect it
-window.addEventListener('resize', () =>{console.log(window.innerWidth);});
-
-
+window.addEventListener("resize", () => {
+  console.log(window.innerWidth);
+});
 
 // gsap configuration
 
-
-
 // gsap animations
-const tl = gsap.timeline({stagger: true})
-tl 
-    .to('.img', {
-        // opacity: 0,
-        // duration: 2,
-        // scale: 1.1,
-        // rotate:10,
-        // ease: "power2.out"
-    })
-    .to('.img', {
-        duration: 3,
-        scale: 1.05,
-        rotate: 10,
-        ease: 'power2.out'
-    })
-    .to('.img', {
-        opacity: 0,
-        onComplete: () => {
-            window.location.href = "src/components/main.html"; // navigate to the next page after animation completes
-          }
-    })
+const tl = gsap.timeline({ stagger: true });
+tl.to(".img", {
+  // opacity: 0,
+  // duration: 2,
+  // scale: 1.1,
+  // rotate:10,
+  // ease: "power2.out"
+})
+  .to(".img", {
+    duration: 3,
+    scale: 1.05,
+    rotate: 10,
+    ease: "power2.out",
+  })
+  .to(".img", {
+    opacity: 0,
+    onComplete: () => {
+      window.location.href = "src/components/main.html"; // navigate to the next page after animation completes
+    },
+  });
